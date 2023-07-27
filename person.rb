b6ad94a29502df3a24d1c872a646b77dfda379f7
berylcl <gakiiberyl@gmail.com>
2023-07-27 06:24:27 +0100

    Making required changes

--- person.rb
+++ person.rb
@@ -1,13 +1,18 @@
 # FrozenStringLiteral: true
 
 class Person
-  attr_reader :id, :name, :age
+  attr_reader :id, :parent_permission
+  attr_accessor :name, :age
 
-  def initialize(name: 'Unknown', age: 18, parent_permission: true)
+  def initialize(name = "Unknown", age = 18, parent_permission = true)
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
