88a628a66511dfad601cae238dd8f0628b1dcede
Gakii Beryl <gakiiberyl@gmail.com>
2023-07-27 21:42:27 +0000

    Update student.rb to inherit from Person and add classroom association

--- student.rb
+++ student.rb
@@ -1,9 +1,10 @@
-# FrozenStringLiteral: true
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, name: 'Unknown', age: 18, parent_permission: true)
-    super(name: name, age: age, parent_permission: parent_permission)
+  attr_accessor :classroom
+
+  def initialize(classroom, age = 18, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
