f09f3b072494f8dc05521adf357fd79d73579c17
Gakii Beryl <gakiiberyl@gmail.com>
2023-07-27 22:59:25 +0000

    Fixing rubocop error

--- main.rb
+++ main.rb
@@ -4,16 +4,16 @@ def main
   app = App.new
 
   loop do
-    puts "\nOptions:"
-    puts "1. List all books"
-    puts "2. List all people"
-    puts "3. Create a person"
-    puts "4. Create a book"
-    puts "5. Create a rental"
-    puts "6. List all rentals for a given person id"
-    puts "7. Quit"
+    puts '\nOptions:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List all rentals for a given person id'
+    puts '7. Quit'
 
-    print "\nEnter your choice (1-7): "
+    print '\nEnter your choice (1-7): '
     choice = gets.chomp.to_i
 
     case choice
@@ -30,10 +30,10 @@ def main
     when 6
       app.list_rentals_for_person
     when 7
-      puts "Goodbye!"
+      puts 'Goodbye!'
       break
     else
-      puts "Invalid choice. Please try again."
+      puts 'Invalid choice. Please try again.'
     end
   end
 end
