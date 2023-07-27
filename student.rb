71f211db0fedab74a4949127bf1fae249870ebbc
Gakii Beryl <gakiiberyl@gmail.com>
2023-07-27 21:54:01 +0000

    Fixing linters error

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age = 18, name = 'Unknown', parent_permission = true)
+  def initialize(classroom, age = 18, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
