f1b1f0215e4f51e3c9f7bf150ee550518c7da175
Gakii Beryl <gakiiberyl@gmail.com>
2023-07-27 21:43:16 +0000

    update person.rb

--- person.rb
+++ person.rb
@@ -1,19 +1,15 @@
 # FrozenStringLiteral: true
-require_relative 'nameable'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
-
-class Person < Nameable
-  attr_accessor :name, :age, :rentals
+# person.rb
+class Person
+  attr_accessor :name, :age, :rentals, :classroom
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
-    super()
   end
 
   def can_use_services?
@@ -30,12 +26,3 @@ class Person < Nameable
 
   private :of_age?
 end
-
-person = Person.new(22, 'maximilianus')
-person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
