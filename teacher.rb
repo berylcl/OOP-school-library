012fc32901a7854eb5959ea288d59b0423d9e409
Gakii Beryl <gakiiberyl@gmail.com>
2023-07-28 07:51:01 +0000

    Correcting rubocop error

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@
 
 require_relative 'person'
 class Teacher < Person
-  def initialize(name = 'Unknown', age = 18, specialization)
+  def initialize(name, age = 18, specialization = 'Unknown')
     super(age, name)
     @specialization = specialization
   end
