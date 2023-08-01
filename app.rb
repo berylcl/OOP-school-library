7eaa3c740725e261644bbc438b7b218331e6d921
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-01 15:42:08 +0000

    Adding saveable module to app.rb

--- app.rb
+++ app.rb
@@ -1,28 +1,80 @@
+require 'json'
+require 'date'
 require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
 require_relative 'book'
 require_relative 'rental'
 
+module Saveable
+  # Save books data to 'books.json' file.
+  def save_books
+    File.write('books.json', JSON.generate(@books.map { |book| { title: book.title, author: book.author } }))
+  end
+
+  # Save people data to 'people.json' file.
+  def save_people
+    File.write('people.json', JSON.generate(@people.map do |person|
+      {
+        id: person.id,
+        name: person.name,
+        age: person.age,
+        parent_permission: person.instance_variable_get(:@parent_permission),
+        classroom_label: person.classroom&.label
+      }
+    end))
+  end
+
+  # Save rentals data to 'rentals.json' file.
+  def save_rentals
+    File.write('rentals.json', JSON.generate(@rentals.map do |rental|
+      {
+        date: rental.date.to_s,
+        book_title: rental.book.title,
+        book_author: rental.book.author,
+        person_id: rental.person.id
+      }
+    end))
+  end
+end
+
 class App
   attr_accessor :people, :books, :rentals
 
+  include Loadable
+  include Saveable
   def initialize
     @people = []
     @books = []
     @rentals = []
   end
 
+  def load_data
+    load_books
+    load_people
+    load_rentals
+  end
+
+  # Save data to the respective JSON files.
+  def save_data
+    save_books
+    save_people
+    save_rentals
+  end
+
+  # List all people.
   def list_people
     puts 'List of People:'
     @people.each { |person| puts "ID: #{person.id}, #{person.class.name}: #{person.name}" }
   end
 
+  # List all books.
   def list_books
     puts 'List of Books:'
     @books.each_with_index { |book, index| puts "#{index}. #{book.title} by #{book.author}" }
   end
 
+  # Create a new person (student or teacher).
   def create_person
     puts "Enter the person's name:"
     name = gets.chomp
@@ -48,6 +100,7 @@ class App
     end
   end
 
+  # Create a new book.
   def create_book
     puts "Enter the book's title:"
     title = gets.chomp
@@ -60,6 +113,7 @@ class App
     puts "#{title} by #{author} has been added to the library."
   end
 
+  # Create a new rental record.
   def create_rental
     list_people
     puts "Enter the person's id to create a rental:"
@@ -81,6 +135,7 @@ class App
     end
   end
 
+  # List all rentals for a specific person.
   def list_rentals_for_person
     list_people
     puts "Enter the person's id to list their rentals:"
@@ -104,6 +159,7 @@ class App
     end
   end
 
+  # Quit the application.
   def quit
     puts 'Goodbye!'
     exit
