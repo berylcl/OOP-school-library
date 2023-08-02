0d4db0951a244b83c53f5c6e2ce2534a5da6fd95
Gakii Beryl <gakiiberyl@gmail.com>
2023-08-02 16:30:16 +0000

    Fix method delegation

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,8 +1,18 @@
-# FrozenStringLiteral: true
-
 require_relative 'base_decorator'
 
 class CapitalizeDecorator < BaseDecorator
+  def method_missing(method, *args, &block)
+    if @nameable.respond_to?(method)
+      @nameable.send(method, *args, &block)
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(method, include_private = false)
+    @nameable.respond_to?(method, include_private) || super
+  end
+
   def correct_name
     @nameable.correct_name.capitalize
   end
